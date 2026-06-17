82988783ff0c7cfc559bb6064815fafc98b05836
宋文轩 <aizuomaoche@outlook.com>
2026-06-17 18:44:45 +0800

    新增web和修改facebook创建activity，尝试分两类app类和文本类复用UI

---
+++ app/src/main/java/com/swx/dongzhou/Activities/CreateActivities/WebsiteCreateActivity.kt
@@ -0,0 +1,17 @@
+package com.swx.dongzhou.Activities.CreateActivities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.swx.dongzhou.BaseActivity
+import com.swx.dongzhou.R
+import com.swx.dongzhou.databinding.ActivityWebsiteCreateBinding
+
+class WebsiteCreateActivity: BaseActivity<ActivityWebsiteCreateBinding>(
+    ActivityWebsiteCreateBinding::inflate){
+    override fun initView() {
+    }
+
+}
