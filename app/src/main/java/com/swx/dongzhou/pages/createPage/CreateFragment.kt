25594c2917815daf0f2ee1a1392bc6a670e1ee35
宋文轩 <aizuomaoche@outlook.com>
2026-06-17 11:44:30 +0800

    MainActivity底边栏粗糙实现

---
+++ app/src/main/java/com/swx/dongzhou/pages/createPage/CreateFragment.kt
@@ -0,0 +1,15 @@
+package com.swx.dongzhou.pages.createPage
+
+import com.swx.dongzhou.BaseFragment
+import com.swx.dongzhou.databinding.CreateFragmentBinding
+
+class CreateFragment: BaseFragment<CreateFragmentBinding>(CreateFragmentBinding::inflate) {
+    override fun initView() {
+
+    }
+
+    override fun loadData() {
+
+    }
+
+}
